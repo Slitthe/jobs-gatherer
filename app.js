27bba167beec33bbfd0b0c0342b27ec5a9b1fcca
Slitthe <sil.gherman@gmail.com>
2018-03-22 21:17:31 +0200

    Added partial bestjobs parser

--- app.js
+++ app.js
@@ -20,26 +20,53 @@ var getUrls = function() {
          url = urls.ejobs + city + '/' + query + '/page' + page + '/'  
       }
       return url;
-   }
+   };
+
    return {
       reqUrls: reqUrls
    }
 }();
 
-request('https://www.ejobs.ro/locuri-de-munca/bucuresti/web%20dev/page1/', function(err, res, data) {
-   data = data;
-   var hrefExp = /href=('|")(.*?)('|")/gi;
-   var nameExp = />(.*?)(<\/a>)/gi;
-   var items = data.match(/datalayeritemlink.*?>(.*?)(<\/a>)/gi);
-   items = items.map(function(curr) {
-      curr.replace();
-      var href = curr.match(hrefExp)[0];
-      href = href.replace(hrefExp, '$2')
-      var name = curr.match(nameExp)[0];
-      name = name.replace(nameExp, '$1');
-      return {name: name, href: href};
-   });
-});
+var parsers = function() {
+   var ejobs = function(htmlString) {
+      var hrefExp = /href=('|")(.*?)('|")/gi;
+      var nameExp = />(.*?)(<\/a>)/gi;
+      var items = htmlString.match(/datalayeritemlink.*?>(.*?)(<\/a>)/gi);
+      items = items.map(function (curr) {
+         curr.replace();
+         var href = curr.match(hrefExp)[0];
+         href = href.replace(hrefExp, '$2')
+         var name = curr.match(nameExp)[0];
+         name = name.replace(nameExp, '$1');
+         return { name: name, href: href };
+      });
+      return items;
+   };
+
+   var bestjobs = function(htmlString) {
+      htmlString = data.replace(/(\n|\r)/gim, ' ');
+
+      var hrefExp = /href=('|")(.*?)\?/gi;
+      var nameExp = /<strong>(.*?)(<\/strong>)/gi;
+
+      var items = data.match(/job-title.*?(<\/a>)/gim);
+      items = items.map(function (curr) {
+         var href = curr.match(hrefExp)[0];
+         href = href.replace(hrefExp, '$2')
+         var name = curr.match(nameExp)[0];
+         name = name.replace(nameExp, '$1');
+         return { name: name, href: href };
+      });
+      return items;
+   };
+
+   var parse = function(htmlStr) {
+      // Gets rid of new lines to 'match' properly
+      htmlStr = htmlStr.replace(/(\n|\r)/gim, ' ');
+   }
+}
+
+
 
 
 
@@ -52,3 +79,6 @@ app.get('/', function(req, res) {
 app.listen(3000, function() {
    console.log('The app has started');
 });
+
+
+
